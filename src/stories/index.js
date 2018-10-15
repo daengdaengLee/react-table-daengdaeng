dae4f4040179ea0c39e8cc319ad870cdc0f4880f
daengdaengLee <gunho1020@gmail.com>
2018-10-15 13:06:30 +0900

    TST: Make fixed-cols story

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import DefaultTable from './default';
+import FixedColsTable from './fixed-cols';
 
-storiesOf('Basic', module).add('default', () => <DefaultTable />);
+storiesOf('Basic', module)
+  .add('default', () => <DefaultTable />)
+  .add('fixed-cols', () => <FixedColsTable />);
