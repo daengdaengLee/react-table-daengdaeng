762b33e3d388729e16df607d6cd2ea13bc62d324
daengdaengLee <gunho1020@gmail.com>
2018-10-16 10:02:09 +0900

    TST: Register margin-horizontal story to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import DefaultTable from './default';
 import FixedColsTable from './fixed-cols';
+import MarginHorizontalTable from './margin-horizontal';
 
 storiesOf('Basic', module)
   .add('default', () => <DefaultTable />)
-  .add('fixed-cols', () => <FixedColsTable />);
+  .add('fixed-cols', () => <FixedColsTable />)
+  .add('margin-horizontal', () => <MarginHorizontalTable />);
