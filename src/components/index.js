276ff16ff263c7a5d434902fd5521bf3dda1ea92
daengdaengLee <gunho1020@gmail.com>
2018-11-01 16:16:15 +0900

    ENH: Make virtualized table component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Cell from './1-atoms/cell';
 import Table from './2-molecules/table';
+import VirtualizedTable from './2-molecules/virtualized-table';
 
-export { Table, Cell };
+export { Table, Cell, VirtualizedTable };
